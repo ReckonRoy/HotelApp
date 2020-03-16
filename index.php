64e29b1b0afce61e6b4f8d11974df56aabebfafb
Reckon Roy <jongweleroy@yahoo.com>
2020-03-16 10:12:41 +0200

    specified each page description and authors

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-
+/*
+ * 
+ * This is a group project ...Hotel App development constisting of three individuals 
+ * web app is aimed at comparing and booking a hotel reservation
+ * 
+ */
 ?>
--- process.php
+++ process.php
@@ -1,3 +1,10 @@
 <?php
-
+    /*
+     * process.php handles the processing of all user inputs, thats retrieving 
+     * data and sending out data and throws certain responses
+     * 
+     * @version php v7.1
+     * @auther Le-Roy
+     * 
+     */
 ?>
